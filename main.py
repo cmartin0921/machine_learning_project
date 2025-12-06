bfaf6075611ae5504eb20f33bfe94316425a5ad3
Christian <christianmartin209@gmail.com>
2025-12-06 16:17:06 +0100

    data preprocessing files

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from ml_project.data import load_raw_data
+from ml_project.data import load_raw_data, clean_data
 
 def main():
     print(load_raw_data())
+    print(clean_data())
 
 if __name__ == "__main__":
     main()
