61fa6f63e5bb4903e0b8ff4f421018c57e905f4f
Christian <christianmartin209@gmail.com>
2025-12-27 19:32:41 +0100

    changed function name

--- src/ml_project/data/__init__.py
+++ src/ml_project/data/__init__.py
@@ -1,9 +1,9 @@
-from .load_raw_data import load_raw_data
+from .load_data import load_data
 from .clean_data import clean_data
 from .openaq_extract_data import openaq_extract_data
 
 __all__ = [
-    "load_raw_data",
+    "load_data",
     "clean_data",
     "openaq_extract_data"
 ]
