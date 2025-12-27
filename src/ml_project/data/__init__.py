9d2c29c7c73c41ec8e6f773e2927514a13d75ac9
Christian <christianmartin209@gmail.com>
2025-12-27 22:51:54 +0100

    removed load data function

--- src/ml_project/data/__init__.py
+++ src/ml_project/data/__init__.py
@@ -1,9 +1,7 @@
-from .load_data import load_data
 from .clean_data import clean_data
 from .openaq_extract_data import openaq_extract_data
 
 __all__ = [
-    "load_data",
     "clean_data",
     "openaq_extract_data"
 ]
--- src/ml_project/data/load_data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def load_raw_data():
-    pass
