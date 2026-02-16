29ac49024879a6987a8fb8f18b22ee19ecb3133a
Christian Francis Martin <32693242+cmartin0921@users.noreply.github.com>
2026-02-16 23:14:17 +0100

    Model validation (#10)
    * created functions for model validation
    
    * added create_model
    
    * have starter functions ingrained to main script
    
    * using evaluate_model in main
    
    * changed params passed for evaluate_model
    
    * added train_test_split function
    
    * added train_test_split functionality
    
    * using split data
    
    * updated playground area

---
+++ src/ml_project/feature_engineering/train_test_split.py
@@ -0,0 +1,8 @@
+# Import classes and function from sklearn
+
+def train_test_split(df, train_test_split=0.3):
+
+    x_train, x_test = None, None
+    y_train, y_test = None, None
+
+    return x_train, x_test, y_train, y_test
